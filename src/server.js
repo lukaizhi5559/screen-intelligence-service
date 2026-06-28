7b074d3d13159711587f0393440d451f841ecbb4
lukaizhi5559 <cakers5559@gmail.com>
2026-06-28 05:31:05 -0400

    Update to scree capture logi

--- src/server.js
+++ src/server.js
@@ -298,5 +298,6 @@ function gracefulShutdown(signal) {
 
 process.on('SIGTERM', () => gracefulShutdown('SIGTERM'));
 process.on('SIGINT', () => gracefulShutdown('SIGINT'));
+process.on('SIGHUP', () => logger.info('SIGHUP received — ignoring (background process)'));
 
 export default app;
